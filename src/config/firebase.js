fd09bb92aaf30d4487b9e75aa8a12450d008194d
deekshith786 <deekshith.m@comakeit.com>
2023-02-28 22:32:04 +0530

    firebase integration

---
+++ src/config/firebase.js
@@ -0,0 +1,21 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAg8NhrQY1mF-5pGF7JbSvhk6s3_UYmngg",
+    authDomain: "linkedin-9e1eb.firebaseapp.com",
+    projectId: "linkedin-9e1eb",
+    storageBucket: "linkedin-9e1eb.appspot.com",
+    messagingSenderId: "299205073958",
+    appId: "1:299205073958:web:ca5ef56cdc446cb2fc1ae7",
+    measurementId: "G-FM1T6CZEW9"
+  };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  const db = firebaseApp.firestore();
+  const auth = firebase.auth();
+
+  export { db, auth };
--- src/feed/Feed.js
+++ src/feed/Feed.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../feed/Feed.css';
 import CreateIcon from '@mui/icons-material/Create';
 import InputOption from '../inputOption/InputOption';
@@ -6,16 +6,50 @@ import ImageIcon from '@mui/icons-material/Image';
 import SubscriptionsIcon from '@mui/icons-material/Subscriptions';
 import EventNoteIcon from '@mui/icons-material/EventNote';
 import CalendarViewDayIcon from '@mui/icons-material/CalendarViewDay';
+import Post from '../post/Post';
+import { db } from '../config/firebase';
+import firebase from 'firebase/compat/app';
+import { doc, onSnapshot, collection, query, where, onSnapshot } from "firebase/firestore";
+
 
 function Feed() {
+
+    const [input, setInput] = useState('');
+
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        db.collection("posts").onSnapShot((snapshot) => (
+            setPosts(snapshot.docs.map((doc) => ({
+                    id: doc.id,
+                    data: doc.data(),
+                }))
+            )
+        )
+        );
+    }, []);
+
+    const sendPost = (e) => {
+      /**to prevent refresh when click enter */
+      e.preventDefault();
+
+      db.collection('posts').add({
+        name: "Deekshith",
+        description: 'this is despppp',
+        message: input,
+        photoUrl: "",
+        timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+      });
+    };
+
   return (
     <div className='feed'>
       <div className="feed__inputContainer">
         <div className="feed__input">
             <CreateIcon />
             <form> 
-                <input type="text" />
-                <button type='submit'>Send</button>
+                <input value={input} onChange={e => setInput(e.target.value)} type="text" />
+                <button onClick={sendPost} type='submit'>Send</button>
             </form>
         </div>
         <div className="feed__inputOptions">
@@ -26,7 +60,15 @@ function Feed() {
         </div>
       </div>
 
-      {/** Posts */}
+      {posts.map(({ id, data: { name, description, message, photoUrl } }) => (
+        <Post
+            key={id}
+            name={name}
+            description={description}
+            message={message}
+            photoUrl={photoUrl}
+        />
+      ))}
     </div>
   )
 }
