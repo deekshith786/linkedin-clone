f837a019d0be4228ffecb8feb9a59111b78ffd4b
deekshith786 <deekshith.m@comakeit.com>
2023-03-02 19:58:33 +0530

    network sidebar

--- src/network/NetworkSidebar.js
+++ src/network/NetworkSidebar.js
@@ -4,13 +4,13 @@ import { useSelector } from "react-redux";
 import { selectUser } from "../features/userSlice";
 import "../network/NetworkSidebar.css";
 import GroupIcon from "@mui/icons-material/Group";
-import ContactPageIcon from '@mui/icons-material/ContactPage';
-import PersonIcon from '@mui/icons-material/Person';
-import GroupsIcon from '@mui/icons-material/Groups';
-import TodayIcon from '@mui/icons-material/Today';
-import DescriptionIcon from '@mui/icons-material/Description';
-import NewspaperIcon from '@mui/icons-material/Newspaper';
-import TagIcon from '@mui/icons-material/Tag';
+import ContactPageIcon from "@mui/icons-material/ContactPage";
+import PersonIcon from "@mui/icons-material/Person";
+import GroupsIcon from "@mui/icons-material/Groups";
+import TodayIcon from "@mui/icons-material/Today";
+import DescriptionIcon from "@mui/icons-material/Description";
+import NewspaperIcon from "@mui/icons-material/Newspaper";
+import TagIcon from "@mui/icons-material/Tag";
 
 function NetworkSidebar() {
   const user = useSelector(selectUser);
@@ -22,53 +22,73 @@ function NetworkSidebar() {
     </div>
   );
 
-  const min = 1;
-  const max = 500;
-  const random1 = Math.floor(Math.random() * (max - min + 1)) + min;
-  const random2 = Math.floor(Math.random() * (max - min + 1)) + min;
+  const random = () => {
+    const min = 1;
+    const max = 500;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
 
   return (
     <div className="networksidebar">
       <div className="networksidebar__stats">
+        <h3>Manage my network</h3>
         <div className="networksidebar__stat">
-          <GroupIcon />
-          <p>Connections</p>
-          <p className="networksidebar__statNumber">{random1}</p>
+          <div className="networksidebar__icons">
+            <GroupIcon />
+            <p>Connections</p>
+          </div>
+          <p className="networksidebar__statNumber">{random()}</p>
         </div>
         <div className="networksidebar__stat">
-          <ContactPageIcon/>
-          <p>Contacts</p>
-          <p className="networksidebar__statNumber">{random2}</p>
+          <div className="networksidebar__icons">
+            <ContactPageIcon />
+            <p>Contacts</p>
+          </div>
+          <p className="networksidebar__statNumber">{random()}</p>
+        </div>
+
+        <div className="networksidebar__stat">
+          <div className="networksidebar__icons">
+            <PersonIcon />
+            <p>Following & follwers</p>
+          </div>
+          <p className="networksidebar__statNumber">{random()}</p>
+        </div>
+
+        <div className="networksidebar__stat">
+          <div className="networksidebar__icons">
+            <GroupsIcon />
+            <p>Groups</p>
+          </div>
+          <p className="networksidebar__statNumber">{random()}</p>
         </div>
         <div className="networksidebar__stat">
-          <PersonIcon/>
-          <p>Following & follwers</p>
-          <p className="networksidebar__statNumber">{random2}</p>
+          <div className="networksidebar__icons">
+            <TodayIcon />
+            <p>Event</p>
+          </div>
+          <p className="networksidebar__statNumber">{random()}</p>
         </div>
         <div className="networksidebar__stat">
-          <GroupsIcon/>
-          <p>Groups</p>
-          <p className="networksidebar__statNumber">{random2}</p>
+          <div className="networksidebar__icons">
+            <DescriptionIcon />
+            <p>Pages</p>
+          </div>
+          <p className="networksidebar__statNumber">{random()}</p>
         </div>
         <div className="networksidebar__stat">
-          <TodayIcon/>
-          <p>Event</p>
-          <p className="networksidebar__statNumber">{random2}</p>
+          <div className="networksidebar__icons">
+            <NewspaperIcon />
+            <p>Newsletters</p>
+          </div>
+          <p className="networksidebar__statNumber">{random()}</p>
         </div>
         <div className="networksidebar__stat">
-          <DescriptionIcon/>
-          <p>Pages</p>
-          <p className="networksidebar__statNumber">{random2}</p>
-        </div>
-        <div className="networksidebar__stat">
-          <NewspaperIcon/>
-          <p>Newsletters</p>
-          <p className="networksidebar__statNumber">{random2}</p>
-        </div>
-        <div className="networksidebar__stat">
-          <TagIcon/>
-          <p>Hashtags</p>
-          <p className="networksidebar__statNumber">{random2}</p>
+          <div className="networksidebar__icons">
+            <TagIcon />
+            <p>Hashtags</p>
+          </div>
+          <p className="networksidebar__statNumber">{random()}</p>
         </div>
       </div>
       <div className="networksidebar__bottom">
